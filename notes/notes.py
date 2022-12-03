b4f89367545c9cac0a7df184aba063ffa6d97d85
MabeDiaby <diabymabe@gmail.com>
2022-12-02 22:10:45 -0500

    oop coffee machine

--- notes/notes.py
+++ notes/notes.py
@@ -1,13 +1,22 @@
-# Object Oriented Programing - OOP
+# # Object Oriented Programing - OOP
 
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("green")
-timmy.forward(100)
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("green")
+# timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charamnder"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
